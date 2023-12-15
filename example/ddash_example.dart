8d0fa90c2966e1bede4944edadc188ccdd3ebdb0
CalogeroCantone <calolavendetta@gmail.com>
2023-12-15 15:47:34 +0100

    Write first example.

--- example/ddash_example.dart
+++ example/ddash_example.dart
@@ -1,6 +1,6 @@
 import 'package:ddash/ddash.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  final words = 'Hello world'.words();
+  print(words); // ['Hello', 'world'];
 }
